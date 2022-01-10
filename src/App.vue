cbe97c761c71fafccf36f544f863a0544dee8451
sunkesen <277908391@qq.com>
2022-01-10 22:49:50 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<!--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-->
+<!--277908391@qq.com-->
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// git clone https://github.com/sunkesen/supermall.git
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
